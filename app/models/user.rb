a1eb40c20c63dbad4ef8eafdccfbc7095777472a
onomatope2580 <79963946+onomatope2580@users.noreply.github.com>
2021-03-19 21:09:05 +0900

    userモデル修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :birth, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birth
+  end
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
     validates :last_name
