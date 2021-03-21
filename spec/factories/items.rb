906e57cb5175cd65b445dcd453f33f0eba97b925
onomatope2580 <79963946+onomatope2580@users.noreply.github.com>
2021-03-21 20:04:24 +0900

    単体テストコード実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :item do
+    name {"a"}
+    description {"a"}
+    category_id {"2"}
+    status_id {"2"}
+    delivery_fee_id {"2"}
+    delivery_from_id {"2"}
+    delivery_day_id {"2"}
+    price {"300"}
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/hero.jpg'), filename: 'hero.jpg')
+    end
     
   end
 end
