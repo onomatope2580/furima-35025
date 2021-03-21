0bb259fd2dc826fd55bdff62151ceabcd4c8a365
onomatope2580 <79963946+onomatope2580@users.noreply.github.com>
2021-03-21 17:12:49 +0900

    商品登録機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,9 +4,25 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 
   def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :delivery_fee_id, :delivery_from_id, :delivery_day_id, :price).merge(user_id: current_user.id)
+  end
+
 end
