c2c9f3f9b6d88c191d316cc78fea69e64a68c290
onomatope2580 <79963946+onomatope2580@users.noreply.github.com>
2021-03-21 13:15:16 +0900

    モデルのバリデーション設定、アソシエーションの定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,18 @@
 class Item < ApplicationRecord
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :category_id
+    validates :status_id
+    validates :delivery_fee_id
+    validates :delivery_from_id
+    validates :delivery_day_id
+    validates :price
+    validates :user, foreign_key: true
+  end
+
+  belongs_to :user
+  has_one_attached :image
+
 end
