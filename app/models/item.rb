d645fcbed587fadf2df0644999bd9a026c0bc111
onomatope2580 <79963946+onomatope2580@users.noreply.github.com>
2021-03-21 17:12:00 +0900

    active_hashのファイル作成、アソシエーション、バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,15 +3,25 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :description
+    validates :price, format: { with: /[3-9][0-9]{2}|[1-9][0-9]{3,6}/ }
+  end
+
+  with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :status_id
     validates :delivery_fee_id
     validates :delivery_from_id
     validates :delivery_day_id
-    validates :price, format: { with: /[3-9][0-9]{2}|[1-9][0-9]{3,6}/ }
   end
 
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_fee
+  belongs_to :delivery_from
+  belongs_to :delivery_day
 
 end
