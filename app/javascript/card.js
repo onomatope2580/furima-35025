3306c40359237b645c2f18dd009e11d13d0765d4
onomatope2580 <79963946+onomatope2580@users.noreply.github.com>
2021-03-25 17:52:45 +0900

    Javascriptとビューの編集

---
+++ app/javascript/card.js
@@ -0,0 +1,26 @@
+const pay = () => {
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
+  const form = document.getElementById("charge-form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+
+    const card = {
+      number: formData.get("purchase_address[card_number]"),
+      exp_month: formData.get("purchase_address[card_exp_month]"),
+      exp_year: `20${formData.get("purchase_address[card_exp_year]")}`,
+      cvc: formData.get("purchase_address[card_cvc]"),
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        const token000 = `<input value=${token} name='token' type="hidden">`;
+        form.insertAdjacentHTML("beforeend", token000);
+      }
+    });
+  });
+};
+
+window.addEventListener("load", pay);
